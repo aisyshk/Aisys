ca2b3b2b2a363ca49177aed2bd7f8e24f15a149b
aisys <alice@aisys.dev>
2023-04-09 18:54:50 -0600

    id utility

---
+++ src/Aisys.Utilities/IdUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aisys.Utilities
+{
+    public class IdUtility
+    {
+        public static string AlphaNumericalByLength(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+            return new string(Enumerable.Repeat(chars, length).Select(x => x[new Random().Next(x.Length)]).ToArray());
+        }
+    }
+}
