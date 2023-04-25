2343ffb3ea11a57a8dcb6e91c511a6cd63a3fc91
aisys <alice@aisys.dev>
2023-04-25 00:04:05 -0600

    added json util

---
+++ src/Aisys.Utilities/Json.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aisys.Utilities
+{
+    public class Json
+    {
+        public JArray ParseJson(string json)
+        {
+            if (json == null)
+                return new JArray { "JSON Empty" };
+
+            var parsed = JsonConvert.DeserializeObject<JArray>(json);
+            return parsed;
+        }
+    }
+}
