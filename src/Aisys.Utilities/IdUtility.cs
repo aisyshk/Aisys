b9c9bed61812720793ffbb05a0918df53e39c2d5
Alice H <alice@aisys.dev>
2023-05-17 09:59:59 +0200

    added custom authorization id

--- src/Aisys.Utilities/IdUtility.cs
+++ src/Aisys.Utilities/IdUtility.cs
@@ -14,5 +14,16 @@ namespace Aisys.Utilities
 
             return new string(Enumerable.Repeat(chars, length).Select(x => x[new Random().Next(x.Length)]).ToArray());
         }
+        
+        public int AuthorizationId()
+        {
+            int id1 = new Random().Next(0, 99999999);
+            int id2 = new Random().Next(0, 33);
+            int id3 = new Random().Next(0, 873171);
+
+            int idTotal = id1 + id2 + id3;
+
+            return int.MaxValue - idTotal;
+        }
     }
 }
